944b71794dd56544eeaff97adfbe1eda8ac7e6b9
c-jwjw <wldnjschl0901@naver.com>
2024-05-16 11:58:38 +0900

    로그인_닉네임 등록, 조회, 수정, 삭제 구현

---
+++ src/main/java/com/group/laxyapp/dto/user/request/UserDeleteRequest.java
@@ -0,0 +1,15 @@
+package com.group.laxyapp.dto.user.request;
+
+public class UserDeleteRequest {
+
+    private long id;
+    private String nickname;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
---
+++ src/main/java/com/group/laxyapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.group.laxyapp.dto.user.request;
+
+public class UserUpdateRequest {
+
+    private long id;
+    private String nickname;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
+
