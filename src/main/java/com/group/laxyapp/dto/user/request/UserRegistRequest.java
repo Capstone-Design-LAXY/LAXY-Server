791efda4b3ca5b2be370dc7dbc891a2bd4e05129
c-jwjw <wldnjschl0901@naver.com>
2024-05-28 03:37:15 +0900

    age, gender 속성 추가

--- src/main/java/com/group/laxyapp/dto/user/request/UserRegistRequest.java
+++ src/main/java/com/group/laxyapp/dto/user/request/UserRegistRequest.java
@@ -3,8 +3,18 @@ package com.group.laxyapp.dto.user.request;
 public class UserRegistRequest {
 
     private String nickname;
+    private Integer age;
+    private String gender;
 
     public String getNickname() {
         return nickname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
--- src/main/java/com/group/laxyapp/dto/user/response/UserResponse.java
+++ src/main/java/com/group/laxyapp/dto/user/response/UserResponse.java
@@ -1,12 +1,25 @@
 package com.group.laxyapp.dto.user.response;
 
+import com.group.laxyapp.domain.user.User;
+
 public class UserResponse {
     private long id;
     private String nickname;
+    private Integer age;
+    private String gender;
 
-    public UserResponse(long id, String nickname) {
+    public UserResponse(long id, String nickname, Integer age, String gender) {
         this.id = id;
         this.nickname = nickname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.nickname = user.getNickname();
+        this.age = user.getAge();
+        this.gender = user.getGender();
     }
 
     public long getId() {
@@ -15,5 +28,13 @@ public class UserResponse {
 
     public String getNickname() {
         return nickname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
